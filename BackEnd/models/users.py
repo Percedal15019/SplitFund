0aa43e65cd0046321d62beb3c11b7e026b30026a
Abhishek-Satyarum <satyarumabhishek@gmail.com>
2025-11-25 15:39:19 +0530

    Create User model for database

---
+++ BackEnd/models/users.py
@@ -0,0 +1,10 @@
+# backend/models/users.py
+
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)

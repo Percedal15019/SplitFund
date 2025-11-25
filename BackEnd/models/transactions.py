3a8c710e9a2bec7ddd886b6a9c4bd6c31632adab
Abhishek-Satyarum <satyarumabhishek@gmail.com>
2025-11-25 15:40:16 +0530

    Add Transaction model for database

---
+++ BackEnd/models/transactions.py
@@ -0,0 +1,15 @@
+# backend/models/transactions.py
+
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True)
+    group_id = Column(Integer)
+    payer = Column(String)
+    participants = Column(String)  # CSV string: "A,B,C"
+    total_amount = Column(Integer)
+    split_type = Column(String)    # equal / ratio
+    details = Column(String)       # extra JSON-like notes
